100f4fac4467a8653f33bc464f90dd137f1c165b
Efim Poberezkin <efim@poberezkin.ru>
2017-11-17 13:40:14 +0400

    Revert "Made yet more changes"
    This reverts commit 8c6c2d8c75cf201f0d7fbc2646c0d82b934f4186.
    
    Conflicts:
    	src/main/java/com/epam/homework/vcs/App.java

--- src/main/java/com/epam/homework/vcs/App.java
+++ src/main/java/com/epam/homework/vcs/App.java
@@ -6,7 +6,6 @@ public class App {
         System.out.println("Hello world");
         System.out.println("Hello again");
         System.out.println("Hello yet again");
-        System.out.println("Hi");
         System.out.println("Hi again");
     }
 }
