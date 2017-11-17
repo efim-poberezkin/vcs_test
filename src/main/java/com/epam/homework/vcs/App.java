ba5ff2e77e2f85219a42e829baaa147c9d148963
Efim Poberezkin <efim@poberezkin.ru>
2017-11-17 11:52:56 +0400

    Made more changes

--- src/main/java/com/epam/homework/vcs/App.java
+++ src/main/java/com/epam/homework/vcs/App.java
@@ -5,5 +5,6 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Hello world");
         System.out.println("Hello again");
+        System.out.println("Hello yet again");
     }
 }
